a32f854c75ea48117377685624db9452fe0254af
zanetowell <zanetowell@gmail.com>
2023-05-17 17:38:13 -0400

    adds compononents and pages directories

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
+import Nav from './components/Nav';
+import Dashboard from './pages/Dashboard';
+import Welcome from './pages/Welcome';
+import Main from './components/Main';
+
 
 function App() {
   return (
     <div className="App">
+      <Nav />
+      <Main />
     </div>
   );
 }
